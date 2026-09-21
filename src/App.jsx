98a726258afe206cf8e7ab238311c2728f1c6efd
Murilo <murilo@reguardions.com>
2026-09-21 11:44:21 -0300

    Add custom responsive footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
 import { useEffect, useState } from "react";
 import AdminPage from "./pages/AdminPage";
 import AnnouncementBar from "./components/global/AnnouncementBar";
+import Footer from "./sections/global/Footer";
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
-import CartDrawer from "./components/cart/CartDrawer";
-import { CartProvider, useCart } from "./hooks/useCart";
-
-function CartDrawerContainer() {
-  const cart = useCart();
-
-  return (
-    <CartDrawer
-      isOpen={cart.isOpen}
-      onClose={cart.close}
-      onAdd={cart.addItem}
-      items={cart.items}
-      onRemoveItem={cart.removeItem}
-      onUpdateQuantity={cart.updateQuantity}
-      couponCode={cart.couponCode}
-      onCouponCodeChange={cart.setCouponCode}
-      onApplyCoupon={cart.applyCoupon}
-      discount={cart.discount}
-      total={cart.total}
-      remainingForGift={cart.remainingForGift}
-      giftProgress={cart.giftProgress}
-    />
-  );
-}
 
 export default function App() {
   const [pathname, setPathname] = useState(window.location.pathname);
@@ -48,10 +25,10 @@ export default function App() {
     : <Home />;
 
   return (
-    <CartProvider>
+    <>
       <AnnouncementBar />
       {page}
-      <CartDrawerContainer />
-    </CartProvider>
+      <Footer />
+    </>
   );
 }
